08b16fa94cee77891af8425c81131950327791ef
SufferingOnion <indra.asura2014@gmail.com>
2020-07-07 23:14:15 +0300

    fix img path

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
     pictures: [
       {
         name: "«Рождение Венеры»",
-        url: "/img/Venera's_burth.png",
+        url: "/Redsoft_test/img/Venera's_burth.png",
         author: "Сандро Боттичелли",
         last_cost: "2 000 000 $",
         cost: "1 000 000 $",
@@ -17,7 +17,7 @@ export default new Vuex.Store({
       },
       {
         name: "«Тайная вечеря»",
-        url: "/img/Last_supper.png",
+        url: "/Redsoft_test/img/Last_supper.png",
         author: "Леонардо да Винчи",
         last_cost: "0",
         cost: "3 000 000 $",
@@ -26,7 +26,7 @@ export default new Vuex.Store({
       },
       {
         name: "«Сотворение Адама»",
-        url: "/img/Adam's _creations.png",
+        url: "/Redsoft_test/img/Adam's _creations.png",
         author: "Микеланджело",
         last_cost: "6 000 000 $",
         cost: "5 000 000 $",
@@ -35,7 +35,7 @@ export default new Vuex.Store({
       },
       {
         name: "«Урок анатомии»",
-        url: "/img/Anatomy_lesson.png",
+        url: "/Redsoft_test/img/Anatomy_lesson.png",
         author: "Рембрандт",
         last_cost: "0",
         cost: "0",
